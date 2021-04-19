7743604778fbdb26edcd67ad901a1c0e2f748dda
gaylonalfano <gaylon.alfano@gmail.com>
2021-04-19 08:27:51 +0800

    Exploring Ethereum smart contracts basics. Stopping at the point where we test our basic frontend app where we fetchGreeting and then setGreeting.

---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.3",
+  // Tell hardhat to compile artifacts inside Vue app
+  paths: {
+    artifacts: "./src/artifacts",
+  },
+  // Deploy to TestNet instead of MainNet (Ethereum)
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,88 @@
 <template>
+  <img alt="Vue logo" src="../assets/logo.png" />
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click="fetchGreeting">Fetch Greeting</button>
+    <button @click="setGreeting">Set Greeting</button>
+    <input v-model="greeting" placeholder="Set greeting" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// @ts-nocheck
+import { defineComponent, ref } from "vue";
+import { ethers } from "ethers";
+import Greeter from "../artifacts/contracts/Greeter.sol/Greeter.json";
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  name: "Home",
+  setup() {
+    // NOTE Key commands:
+    // npx hardhat node
+    // npx hardhat run scripts/deploy.js --network localhost
+    // Let's store Greeter Address we got from npx hardhat ??
+    const greeterAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+    const greeting = ref<string>("");
+
+    async function setGreeting() {
+      // Check that user typed/entered a greeting
+      if (!greeting.value) return;
+      if (typeof window.ethereum !== "undefined") {
+        await requestAccount();
+        // Create another provider so we can make a transaction to the actual blockchain
+        // For this we need a legit signer
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        // Create a new instance of the contract but with the signer this time
+        const contract = new ethers.Contract(
+          greeterAddress,
+          Greeter.abi,
+          signer
+        );
+        const transaction = await contract.setGreeting(greeting.value);
+        // Commit the actual transaction to the blockchain
+        await transaction.wait();
+        fetchGreeting();
+      }
+    }
+
+    async function requestAccount() {
+      // Request account info from Metamask wallet. Prompts user to connect
+      // Can return an Array of accounts as well if you'd like
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+    }
+
+    async function fetchGreeting() {
+      // Check whether Metamask Extension is connected
+      // window.ethereum will be injected into window if so
+      if (typeof window.ethereum !== "undefined") {
+        // Create a new provider
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        // Create a new instance of the contract
+        const contract = new ethers.Contract(
+          greeterAddress,
+          Greeter.abi,
+          provider
+        );
+        try {
+          const data = await contract.greet();
+          console.log("data: ", data);
+        } catch (err) {
+          console.log("Error: ", err);
+        }
+      }
+    }
+
+    return { greeterAddress, greeting, fetchGreeting, setGreeting };
   },
 });
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+}
+</style>
